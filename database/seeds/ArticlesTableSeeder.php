30fb2be1a5166f3f688544061b42f11a43507d54
MarcoB <mrc.bsllt@gmail.com>
2021-02-16 14:21:00 +0100

    creato il nuovo seeder per la tabella "articles"

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Article;
+use Faker\Generator as Faker;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
