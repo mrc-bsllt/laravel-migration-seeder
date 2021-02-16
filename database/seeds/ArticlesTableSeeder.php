34a3f297ba87fd536803910d809174d65a3c708c
MarcoB <mrc.bsllt@gmail.com>
2021-02-16 14:45:43 +0100

    generato automaticamente 20 blog fake

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,8 +11,19 @@ class ArticlesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+      for ($i=0; $i < 20 ; $i++) {
+
+        $newArticle = new Article();
+
+        $newArticle->title = $faker->words(5, true);
+        $newArticle->subtitle = $faker->sentence(5);
+        $newArticle->blog_content = $faker->paragraphs(3, true);
+        $newArticle->author = $faker->name();
+        $newArticle->date_pubblication = $faker->dateTime();
+
+        $newArticle->save();
+      }
     }
 }
